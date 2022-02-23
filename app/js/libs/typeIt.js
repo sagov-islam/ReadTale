fd2ed08db7dc85fdc252285c60e1a7f0b3e2876d
Islam Sagov <sagov-2001@mail.ru>
2022-02-23 21:23:40 +0300

    Js компонент: TypeIt

---
+++ app/js/libs/typeIt.js
@@ -0,0 +1,56 @@
+class TypeIt {
+
+    #letter = 0;
+    #line = 0
+    #result = '';
+
+    #textList;
+    #interval;
+    #dataAtr
+
+    constructor(textList, interval, dataAtr) {
+        this.#textList = textList
+        this.#interval = interval
+        this.#dataAtr = dataAtr
+    }
+
+    start() {
+        this.#timeOut(this.#interval, this.#line, this.#letter)
+    }
+
+    #timeOut(time, line, letter) {
+        const arr = this.#textList;
+        
+        setTimeout(() => {
+            if (line > arr.length - 1) {
+                return
+            }
+
+            if (typeof arr[line] === 'number') {
+                this.#timeOut(arr[line], ++line, letter = 0);
+                return
+            };
+
+            if (letter >= arr[line].length) {
+                this.#timeOut(this.#interval, ++line, letter = 0);
+                return
+            }
+
+            if (typeof arr[line] === 'string') {
+                this.#result += arr[line][letter];
+                this.#render();
+                this.#timeOut(this.#interval, line, ++letter);
+                return
+            }
+            
+        }, time);
+    }
+
+    #render() {
+        $(`[data-typeit-${this.#dataAtr}]`).html(this.#result)
+    }
+
+}
+
+const typeIt = new TypeIt(['Забудь\nнадежду,', 200, ' всяк\nсюда\nвходящий'], 50, 'titleBg')
+typeIt.start()
