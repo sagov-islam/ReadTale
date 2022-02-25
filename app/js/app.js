b27d0d2e2918006a0b6b9b0734139e6d2009017e
Islam Sagov <sagov-2001@mail.ru>
2022-02-25 15:35:38 +0300

    Изменение компонента TypeIt
    Перенос всех свойств конструктора в объект

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,11 @@
-const typeItTitleBg = new TypeIt(['Забудь\nнадежду,', 200, ' всяк\nсюда\nвходящий'], 50, 'titleBg');
+// const typeItTitleBg = new TypeIt(['Забудь\nнадежду,', 200, ' всяк\nсюда\nвходящий'], 50, 'titleBg');
+const typeItTitleBg = new TypeIt({
+    textList: [
+        'Забудь\nнадежду,',
+        200,
+        ' всяк\nсюда\nвходящий'
+    ],
+    interval: 50,
+    dataAtr: 'titleBg'
+});
 typeItTitleBg.start()
