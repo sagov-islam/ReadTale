1b2170f340577cda7224e13f9e987fb16487c336
Islam Sagov <sagov-2001@mail.ru>
2022-02-25 15:26:09 +0300

    Другое название для объекта класса

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,2 @@
-const typeIt = new TypeIt(['Забудь\nнадежду,', 200, ' всяк\nсюда\nвходящий'], 50, 'titleBg')
-typeIt.start()
+const typeItTitleBg = new TypeIt(['Забудь\nнадежду,', 200, ' всяк\nсюда\nвходящий'], 50, 'titleBg');
+typeItTitleBg.start()
