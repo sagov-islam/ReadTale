a77e61289b88e2c0773ea3009c58865ce2641655
Islam Sagov <sagov-2001@mail.ru>
2022-02-27 11:14:30 +0300

    Класс DOMWorker
    Класс для работы с DOM элементами.

---
+++ app/js/libs/domWorker.js
@@ -0,0 +1,23 @@
+export default class DOMWorker {
+
+    static findElementAndReturn(selector, context) {
+        const elementsList = $(selector, context ? context : '');
+
+        if (elementsList.length !== 0) return elementsList;
+        throw new ReferenceError(`element ${selector} is not defined`);
+    }
+
+    static addElement(containerSelector, element) {
+        const container = containerSelector ? $(`${containerSelector}`) : undefined;
+
+        if (!container)
+            throw new ReferenceError(`you have to add container for the element in the addElement()`);
+        if (container.length === 0)
+            throw new ReferenceError(`container '${containerSelector}' not found`);
+        if (!element)
+            throw new ReferenceError(`you have to add element in the addElement()`);
+        
+        container.html(element);
+    }
+}
+
