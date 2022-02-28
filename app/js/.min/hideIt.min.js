1a0057fdbe2ae45ce8661128fc15f62949e2a8c6
Islam Sagov <sagov-2001@mail.ru>
2022-02-28 11:59:57 +0300

    Минификация

--- app/js/.min/hideIt.min.js
+++ app/js/.min/hideIt.min.js
@@ -2,24 +2,59 @@ import DOMWorker from './domWorker.min.js';
 
 export default class HideIt {
 
-    #selector;
-    #callback;
-    constructor(selector, px, seconds, callback) {
-        this.#selector = selector;
-        this.px = px;
-        this.seconds = seconds;
-        this.#callback = callback;
+    #props;
+    constructor(props) {
+        this.#props = props;
+        this.#props.element = DOMWorker.findElementAndReturn(props.selector);
     }
 
-    x() {
-        const element = DOMWorker.findElementAndReturn(this.#selector);
-        element.css({
-            'transition': `${this.seconds}s`,
-            'transform': `translate(${this.px}, 0)`,
+    // ---- PUBLIC ---->
+
+    smoothHideX() {
+        this.#props.element.css({
+            'transition': `${this.#props.seconds}s`,
+            'transform': `translate(${this.#props.px}, 0)`,
             'opacity': '0'
         });
 
-        if (!this.#callback) return
-        setTimeout(this.#callback, (this.seconds * 1000));
+        if (!this.#props.callback) return
+        setTimeout(this.#props.callback, (this.#props.seconds * 1000));
     }
+
+    smoothHideY() {
+        this.#props.element.css({
+            'transition': `${this.#props.seconds}s`,
+            'transform': `translate(0, ${this.#props.px})`,
+            'opacity': '0'
+        });
+
+        if (!this.#props.callback) return
+        setTimeout(this.#props.callback, (this.#props.seconds * 1000));
+    }
+    
+    // <---- PUBLIC ----
+
+    
+    // ---- STATIC ---->
+
+    static hideY(selector, px) {
+        const element = DOMWorker.findElementAndReturn(selector);
+
+        element.css({
+            'transform': `translate(0, ${px})`,
+            'opacity': '0'
+        });
+    }
+
+    static hideX(selector, px) {
+        const element = DOMWorker.findElementAndReturn(selector);
+
+        element.css({
+            'transform': `translate(${px}, 0)`,
+            'opacity': '0'
+        });
+    }
+
+    // <---- STATIC ----
+
 }
