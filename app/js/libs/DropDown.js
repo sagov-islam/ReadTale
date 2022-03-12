4bc94c546f976edd52f5a787fbea30bf0822b60d
Islam Sagov <sagov-2001@mail.ru>
2022-03-12 12:02:26 +0300

    Доработка DropDown

--- app/js/libs/DropDown.js
+++ app/js/libs/DropDown.js
@@ -9,33 +9,50 @@ export default class DropDown {
 
     constructor(props) {
         this.#props = props;
-
         this.#dropdown = DOMWorker.findElementAndReturn(`[data-dropdown=${props.dataAtr}]`);
+        this.selectedInputs = [];
     }
 
     // PUBLIC ---->
     create() {
         const dropdown = this.#dropdown;
         const props = this.#props;
+        const listSize = props.listSize ?  props.listSize : '120%';
+        const buttonSize = 'rt-dropdown-button--size-' + (props.buttonSize === 'big' ? 'big' : props.buttonSize === 'small' ? 'small' : 'normal');
+        
+        dropdown.addClass(`rt-dropdown ${props.classes ? props.classes.join(' ') : ''}`);
 
-        const sizeClass = 'rt-dropdown--size-' + (props.size === 'big' ? 'big' : props.size === 'small' ? 'small' : 'normal');
-        dropdown.addClass(`rt-dropdown ${sizeClass} ${props.classes ? props.classes.join(' ') : ''}`);
-
-        const dropdownList = props.list.map((title) => {
+        const list = props.list.map((text) => {
             return ReturnHTML.dropdownItem({
-                title: title,
-                dataAtr: props.dataAtr
+                title: text,
+                dataAtr: props.dataAtr,
+                inputName: props.inputName
             });
         });
     
         dropdown.html(ReturnHTML.dropdown({
             title: props.title,
-            list: dropdownList,
-            dataAtr: props.dataAtr
+            list: list,
+            dataAtr: props.dataAtr,
+            buttonSize: buttonSize,
+            listSize: listSize
         }));
+
+        const inputs = DOMWorker.findElementAndReturn(`.rt-${props.inputName}__input`, dropdown);
+        $(inputs).on('click', () => { this.#updateSelectedInputs() })
 
         DropDownEventListener.closeIfClickWindow(dropdown);
         DropDownEventListener.openIfClickButton(dropdown, props.transition);
+    }
+
+    #updateSelectedInputs() {
+        this.selectedInputs = [];
+        const inputs = DOMWorker.findElementAndReturn(`.rt-${this.#props.inputName}__input`, this.#dropdown);
+        
+        inputs.each((index, input) => {
+            if ($(input).is(':checked')) this.selectedInputs.push(input.value);
+            
+        }) 
     }
     // <---- PUBLIC
 }
