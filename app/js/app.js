3c35dc96df2aa50272500d04435451c7b784fab8
Islam Sagov <sagov-2001@mail.ru>
2022-03-12 11:58:31 +0300

    Удаление лишнего кода

--- app/js/app.js
+++ app/js/app.js
@@ -2,8 +2,6 @@ import DOMWorker from './DomWorker.min.js';
 import ReturnHTML from './ReturnHTML.min.js'
 
 $(() => {
-    // HEADER:
-    DOMWorker.prepend('body > .simplebar-wrapper .simplebar-content', ReturnHTML.header());
 });
 
 
