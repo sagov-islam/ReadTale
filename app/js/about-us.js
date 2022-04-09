4f9317f1d282af9abc8347fa5dc4400ace8abc0a
Islam Sagov <sagov-2001@mail.ru>
2022-04-09 14:03:41 +0300

    Js файл для страницы about-us

---
+++ app/js/about-us.js
@@ -0,0 +1,17 @@
+import HideIt from './HideIt.min.js';
+import ShowIt from './ShowIt.min.js';
+
+$(() => {
+
+    const spinner = new HideIt({
+        selector: '.rt-loader__content',
+        px: '50px',
+        seconds: 0.5,
+        callback: () => {
+            $('.rt-loader').remove();
+            new ShowIt({ selector: 'header, section, footer', seconds: 0.5 }).smoothShow();
+        }
+    })  
+    spinner.smoothHideY();
+
+})
